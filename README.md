2d43cace37b23057fdc11e0d4036d3abcbc467ec
veffhz <veffhz@example.com>
2020-12-27 20:16:18 +0300

    Added base project

---
+++ README.md
@@ -0,0 +1,1 @@
+# jira-task-manager 
---
+++ src/test/java/ru/sber/jira/task/manager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sber.jira.task.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
